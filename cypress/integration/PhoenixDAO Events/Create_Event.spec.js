42a7468111da0cc50ff79d1986352cd00bea8f38
AdinaFakih <addy.fakih@gmail.com>
2021-07-19 17:58:48 +0500

    All Steps Completed

--- cypress/integration/PhoenixDAO Events/Create_Event.spec.js
+++ cypress/integration/PhoenixDAO Events/Create_Event.spec.js
@@ -1,5 +1,4 @@
 
-
 describe('Test Create Event', () =>
 {
     before(() => {
@@ -11,16 +10,19 @@ describe('Test Create Event', () =>
 
     it('Create Event',function(){
         cy.visit_Events_Dapps()
-        //cy.log(this.data.Step1.length)
         cy.visit_Create_Events()
         for (let i = 0; i < 1; i++){
-        //this.data.Step1.forEach(elements => {this.data.Step1.length
-            //cy.Step1_One_day_Event(this.data.Step1[i].event_name, this.data.Step1[i].event_organizer,this.data.Step1[i].date,this.data.Step1[i].start_time,this.data.Step1[i].end_time)
-            cy.Step1_More_than_a_day(this.data.Step1[i].event_name,this.data.Step1[i].event_organizer,this.data.Step1[i].start_date,this.data.Step1[i].end_date,this.data.Step1[i].to,this.data.Step1[i].from)
-            //cy.Step2_Phycical_Event(this.data.Step2[i].event_location)
-            //cy.Step2_Online_Event(this.data.Step2[i].event_location)
-            //})
-        cy.wait(3000)
+            cy.Step1_One_day_Event(this.data.info[i].event_name, this.data.info[i].event_organizer)
+            //cy.Step1_More_than_a_day(this.data.info[i].event_name,this.data.info[i].event_organizer)
+            cy.Step2_Physical_Event(this.data.info[i].event_location,this.data.info[i].topic,this.data.info[i].filepath)
+            //cy.Step2_Online_Event(this.data.info[i].event_link)
+            //cy.Step3_Free_Event_unlimited()
+            //cy.Step3_Paid_Single_Event_unlimited(this.data.info[i].ticketPrice)
+            //cy.Step3_Paid_Multiple_Event_unlimited(this.data.info[i].ticketName1,this.data.info[i].ticketName2,this.data.info[i].ticketPrice)
+            //cy.Step3_Free_Event_limited(this.data.info[i].no_of_tickets)
+            //cy.Step3_Paid_Single_Event_limited(this.data.info[i].ticketPrice,this.data.info[i].no_of_tickets)
+            cy.Step3_Paid_Multiple_Event_limited(this.data.info[i].ticketName1,this.data.info[i].ticketName2,this.data.info[i].ticketPrice,this.data.info[i].no_of_tickets)
+            cy.Step4(this.data.info[i].description)
         }
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+import 'cypress-file-upload'
 
 Cypress.Commands.add('visit_Events_Dapps', () => { 
     //Website Address
@@ -13,64 +14,60 @@ Cypress.Commands.add('visit_Create_Events', () => {
     cy.title().should('be.equal','PhoenixDAO Events Marketplace - Buy Tickets to Events in PHNX.')
 })
 
-Cypress.Commands.add('Create_Event_Form', () => { 
-    cy.url().should('include','createevent')
-    cy.Step1_One_day_Event()
-    cy.Step1_More_than_a_day()
-    
-})
+// Cypress.Commands.add('Create_Event_Form', () => { 
+//     cy.url().should('include','createevent')
+//     cy.Step1_One_day_Event()
+//     cy.Step1_More_than_a_day()
+// })
 
-Cypress.Commands.add('Step1_One_day_Event', (event_name,event_organizer,date,start_time,end_time) => { 
-    // cy.title().should('be.equal','PhoenixDAO Events Marketplace - Buy Tickets to Events in PHNX.')
-    // cy.contains('Create Event').should('be.visible')
-    // cy.contains('Event Details').should('be.visible')
+Cypress.Commands.add('Step1_One_day_Event', (event_name,event_organizer) => { 
+    cy.title().should('be.equal','PhoenixDAO Events Marketplace - Buy Tickets to Events in PHNX.')
+    cy.contains('Create Event').should('be.visible')
+    cy.contains('Event Details').should('be.visible')
     cy.get("#event-name").should('be.visible').should('be.enabled').clear({force: true}).type(event_name)
     cy.get("#event-organizer").should('be.visible').should('be.enabled').clear({force: true}).type(event_organizer)
     cy.get('input[value="onedayevent"]').check().should('be.checked').and('have.value','onedayevent')
     //Click on calendar icon
     cy.get(':nth-child(1) > :nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click()
-    // const day = 5
-    // const dayRegex = new RegExp(`^\\b${day}\\b$`)
     //click on date
-    cy.xpath("/html/body/div[2]/div[3]/div/div/div[2]/div/div[4]/div[6]/button/span[1]/p").click()
-    //click on clock icon
+    cy.xpath("/html/body/div[2]/div[3]/div/div/div[2]/div/div[4]/div[6]/button/span[1]/p").should('be.visible').click()
+    //click on clock icon for Start time
     cy.get('[style="display: flex; justify-content: space-between;"] > :nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click()
-    //click on start time (hour)
-    cy.xpath("/html/body/div[2]/div[3]/div/div[1]/div/div[2]/div/div/span[4]").click({force: true})
-    //click on start time (second)
-    cy.xpath("/html/body/div[2]/div[3]/div/div[1]/div/div[2]/div/div/span[6]").click({force: true})
-    //click on end time (hour)
-
-    // cy.get("input[id='event-date']").click()
-    // cy.get("input[id='event-date']").should('be.visible').should('be.enabled').type(date)
-    // cy.get("input[id='start-time-picker']").should('be.visible').should('be.enabled').type(start_time)
-    // cy.get("input[id='end-time-picker']").should('be.visible').should('be.enabled').type(end_time)
-    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").click({force: true}) 
+    //Click on OK button for Start time
+    cy.get('.MuiDialogActions-root > :nth-child(2) > .MuiButton-label').should('be.visible').click()
+    //click on clock icon for End time
+    cy.get(':nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').should('be.visible').click()
+    //click on OK for end time
+    cy.get('.MuiDialogActions-root > :nth-child(2)').should('be.visible').click()
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click() 
 })
 
-Cypress.Commands.add('Step1_More_than_a_day', (event_name,event_organizer,start_date,end_date,to,from) => { 
-    // cy.title().should('eq','PhoenixDAO Events Marketplace - Buy Tickets to Events in PHNX.')
-    // cy.contains('Create Event').should('be.visible')
-    // cy.contains('Event Details').should('be.visible')
+Cypress.Commands.add('Step1_More_than_a_day', (event_name,event_organizer) => { 
+    cy.title().should('eq','PhoenixDAO Events Marketplace - Buy Tickets to Events in PHNX.')
+    cy.contains('Create Event').should('be.visible')
+    cy.contains('Event Details').should('be.visible')
     cy.get("#event-name").should('be.visible').should('be.enabled').clear({force: true}).type(event_name)
     cy.get("#event-organizer").should('be.visible').should('be.enabled').clear({force: true}).type(event_organizer)
-    cy.get('input[value="morethanaday"]').check().should('be.checked').and('have.value','morethanaday')
-    //Click on calendar icon
-    cy.xpath("//body[@classname='background']/div[@id='root']/div[@id='wrapper']/div[@id='page-content-wrapper']/div[@class='page-wrapper-inner']/div[@class='container']/div[@class='retract-page-inner-wrapper']/div[@class='home-wrapper']/div[@class='row']/div[@class='col-xl-8 col-lg-8 col-md-12 col-sm-12 col-xs-12']/div[@class='jss21']/div[@class='jss26']/div/div/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/button[1]/span[1]/*[1]").click()      
-    //click on start date
-    cy.xpath("/html/body/div[2]/div[3]/div/div/div[2]/div/div[4]/div[6]/button/span[1]/p").click()
-    //click on calendar icon
-    //cy.get(':nth-child(1)>:nth-child(1)>.MuiFormControl-root>.MuiInputBase-root>.MuiInputAdornment-root>.MuiButtonBase-root>.MuiIconButton-label>.MuiSvgIcon-root').click()
-    //select date in calendar 
-    //cy.get(':nth-child(1)>:nth-child(1)>.MuiFormControl-root>.MuiInputBase-root>.MuiInputAdornment-root>.MuiButtonBase-root>.MuiIconButton-label>.MuiSvgIcon-root').click()
-    // //cy.get(':nth-child(4) > :nth-child(3) > .MuiButtonBase-root').click()
-    // // cy.get(":nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > #date-picker-inline").type(start_date)
-    // // cy.get(":nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > #date-picker-inline").type(end_date)
-    // cy.get(":nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > #date-picker-inline").click()
-    // //.type(to)
-    // cy.get(':nth-child(3) > :nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click()
-    // cy.get(":nth-child(2) > .MuiInputBase-root > #time-picker").type(from)
-    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").click()
+    cy.get('input[value="morethanaday"]').check().should('be.checked').and('have.value','morethanaday')     
+    //Click on calander icon for Start date 
+    cy.get(':nth-child(1) > :nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').should('be.visible').click()
+    //Click on Start date
+    cy.xpath("/html/body/div[2]/div[3]/div/div/div[2]/div/div[4]/div[3]/button/span[1]/p").should('be.visible').click()
+    //Click on calendar icon for End date 
+    cy.get(':nth-child(1) > :nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').should('be.visible').click()
+    //Click on End date
+    cy.xpath("/html/body/div[2]/div[3]/div/div/div[2]/div/div[4]/div[6]/button/span[1]/p").should('be.visible').click()
+    //Click on Clock icon for To
+    cy.get(':nth-child(3) > :nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').should('be.visible').click()
+    //Click on Ok
+    cy.get('.MuiDialogActions-root > :nth-child(2)').should('be.visible').click()
+    //Click on Clock icon for From
+    cy.get(':nth-child(3) > :nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').should('be.visible').click()
+    //Click on Ok
+    cy.get('.MuiDialogActions-root > :nth-child(2)').should('be.visible').click()
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
 })
 
 Cypress.Commands.add('Step2_Physical_Event', (event_location) => { 
@@ -78,17 +75,134 @@ Cypress.Commands.add('Step2_Physical_Event', (event_location) => {
     cy.contains('Create Event').should('be.visible')
     cy.contains('Event Details').should('be.visible')
     cy.get('input[value="physical"]').check().should('be.checked').and('have.value','physical')
+    cy.get('input[value="online"]').should('not.be.checked').and('have.value','online')
     cy.get("#event-location").should('be.visible').should('be.enabled').type(event_location)
-
-    cy.get("//span[contains(text(),'Browse')]").should('be.visible').should('be.enabled').clear({force: true}).type(event_organizer)
+    //image
+    const filepath = 'images/Banner.jpg'
+    cy.get('input[type="file"]').attachFile(filepath)
+     //Topic dropdown
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Food & Drink').click()
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
 })
 
-Cypress.Commands.add('Step2_Online_Event', (event_location) => { 
+Cypress.Commands.add('Step2_Online_Event', (event_link,topic,filepath) => { 
     cy.title().should('eq','PhoenixDAO Events Marketplace - Buy Tickets to Events in PHNX.')
     cy.contains('Create Event').should('be.visible')
     cy.contains('Event Details').should('be.visible')
     cy.get('input[value="online"]').check().should('be.checked').and('have.value','online')
-    cy.get("#event-location").should('be.visible').should('be.enabled').type(event_location)
-
+    cy.get('input[value="physical"]').should('not.be.checked').and('have.value','physical')
+    cy.get("#event-link").should('be.visible').should('be.enabled').type(event_link)
+    //image
+    cy.get('input[type="file"]').attachFile(filepath)
+    //Topic dropdown
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains(topic).click()
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
 })
 
+Cypress.Commands.add('Step3_Free_Event_unlimited', () => {
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Free Event').click()
+    //TICKET AVAILABILITY
+    cy.get('input[value="unlimited"]').check().should('be.checked').and('have.value','unlimited')
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
+})
+
+Cypress.Commands.add('Step3_Free_Event_limited', (no_of_tickets) => {
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Free Event').click()
+    //TICKET AVAILABILITY
+    cy.get('input[value="limited"]').check().should('be.checked').and('have.value','limited')
+    //NUMBER OF TICKETS
+    cy.get('input[id="outlined-basic"]').should('be.visible').should('be.enabled').type(no_of_tickets)
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
+})
+
+Cypress.Commands.add('Step3_Paid_Single_Event_unlimited', (ticketPrice) => {
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Paid (Single Ticket Type Event)').click()
+    //TICKET PRICE
+    cy.get('#input-with-icon-textfield').should('be.visible').should('be.enabled').type(ticketPrice)
+    //TICKET AVAILABILITY
+    cy.get('input[value="unlimited"]').check().should('be.checked').and('have.value','unlimited')
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click({force: true})
+})
+
+Cypress.Commands.add('Step3_Paid_Single_Event_limited', (ticketPrice,no_of_tickets) => {
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Paid (Single Ticket Type Event)').click()
+    //TICKET PRICE
+    cy.get('#input-with-icon-textfield').should('be.visible').should('be.enabled').type(ticketPrice)
+    //TICKET AVAILABILITY
+    cy.get('input[value="limited"]').check().should('be.checked').and('have.value','limited') 
+    //NUMBER OF TICKETS
+    cy.get('input[id="outlined-basic"]').should('be.visible').should('be.enabled').type(no_of_tickets)
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click({force: true})
+})
+
+Cypress.Commands.add('Step3_Paid_Multiple_Event_unlimited', (ticketName1,ticketName2,ticketPrice) => {
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Paid (Multiple Ticket Type Event)').click()
+    //TICKET NAME
+    cy.get('#ticket-name').should('be.visible').should('be.enabled').type(ticketName1)
+    //TICKET PRICE
+    cy.get('#input-with-icon-textfield').should('be.visible').should('be.enabled').type(ticketPrice)
+    //TICKET AVAILABILITY
+    cy.get('input[value="unlimited"]').check().should('be.checked').and('have.value','unlimited')
+    //Click on Save button
+    cy.xpath('//span[contains(text(),"Save")]').click()
+    //add another ticket
+    cy.get(':nth-child(1) > :nth-child(2) > .MuiButtonBase-root > .MuiButton-label').click()
+    //TICKET NAME
+    cy.get('#ticket-name').should('be.visible').should('be.enabled').type(ticketName2)
+    //TICKET PRICE
+    cy.get('#input-with-icon-textfield').should('be.visible').should('be.enabled').type(ticketPrice)
+    //TICKET AVAILABILITY
+    cy.get('input[value="unlimited"]').check().should('be.checked').and('have.value','unlimited')
+    //Click on Save button
+    cy.xpath('//span[contains(text(),"Save")]').click()
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
+})
+
+Cypress.Commands.add('Step3_Paid_Multiple_Event_limited', (ticketName1,ticketName2,ticketPrice,no_of_tickets) => {
+    cy.get('#demo-simple-select-outlined').click()
+    cy.get('div > ul > li').contains('Paid (Multiple Ticket Type Event)').click()
+    //TICKET NAME
+    cy.get('#ticket-name').should('be.visible').should('be.enabled').type(ticketName1)
+    //TICKET PRICE
+    cy.get('#input-with-icon-textfield').should('be.visible').should('be.enabled').type(ticketPrice)
+    //TICKET AVAILABILITY
+    cy.get('input[value="limited"]').check().should('be.checked').and('have.value','limited')
+    //NUMBER OF TICKETS
+    cy.get('input[id="outlined-basic"]').should('be.visible').should('be.enabled').type(no_of_tickets)
+    //Click on Save button
+    cy.xpath('//span[contains(text(),"Save")]').click()
+    //add another ticket
+    cy.get(':nth-child(1) > :nth-child(2) > .MuiButtonBase-root > .MuiButton-label').click()
+    //TICKET NAME
+    cy.get('#ticket-name').should('be.visible').should('be.enabled').type(ticketName2)
+    //TICKET PRICE
+    cy.get('#input-with-icon-textfield').should('be.visible').should('be.enabled').type(ticketPrice)
+    //TICKET AVAILABILITY
+    cy.get('input[value="limited"]').check().should('be.checked').and('have.value','limited')
+    //NUMBER OF TICKETS
+    cy.get('input[id="outlined-basic"]').should('be.visible').should('be.enabled').type(no_of_tickets)
+    //Click on Save button
+    cy.xpath('//span[contains(text(),"Save")]').click()
+    //Click NEXT button
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
+})
+
+Cypress.Commands.add('Step4', (description) => {
+    cy.get(".public-DraftStyleDefault-block").type(description)
+    cy.get('input[class="jss14"]').check().should('be.checked')
+    cy.get(".jss25 > .MuiButton-contained > .MuiButton-label").should('be.visible').click()
+})
